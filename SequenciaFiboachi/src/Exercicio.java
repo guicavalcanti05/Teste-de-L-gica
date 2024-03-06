8b2e3d316344a7330adfed28d50ca916fde34251
Guilherme Cavalcanti <142848558+guicavalcanti05@users.noreply.github.com>
2024-03-05 22:26:35 -0300

    Update Exercicio.java

--- SequenciaFiboachi/src/Exercicio.java
+++ SequenciaFiboachi/src/Exercicio.java
@@ -5,7 +5,7 @@
 	    public static void main(String[] args) {
 	    Scanner teclado = new Scanner(System.in);
         
-        System.out.print("Digite um número para verificar se pertence à sequência de Fibonacci: ");
+        System.out.print("Digite um número -->");
         int num = teclado.nextInt();
         teclado.close();
 
